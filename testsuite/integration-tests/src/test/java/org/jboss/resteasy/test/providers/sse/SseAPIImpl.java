8605f398d7c2560f0e2f9d3ec18521c57eca1247
Jim Ma <ema@redhat.com>
2020-05-09 20:11:34 +0800

    Add thread sleep

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/sse/SseAPIImpl.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/sse/SseAPIImpl.java
@@ -32,6 +32,12 @@ public class SseAPIImpl implements SseAPI {
         logger.info("********************SSEAPITest********************** : sending message : " + message);
         sseSink.send(sse.newEvent(message));
         logger.info("********************SSEAPITest********************** : message sent");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return message;
     }
 
