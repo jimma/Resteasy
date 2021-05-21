b7beccd4eaf6f7a0c0b2d8b1ffc58891ee231d12
Jim Ma <ema@redhat.com>
2021-05-21 17:38:00 +0800

    Fix travis build and Change to another microprofile-rest-client variant

--- travis-script/prebuild.sh
+++ travis-script/prebuild.sh
@@ -2,15 +2,20 @@
 set -ex
 git clone --depth=1 --branch=ee9-experimental https://github.com/jimma/jboss-jakarta-jaxrs-api_spec.git $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec
 pushd $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec/jaxrs-api
-mvn --quiet clean install -DskipTest=true
+mvn clean install -DskipTests=true
 popd
 
 git clone --depth=1 --branch=main https://github.com/jimma/optimus.git $TRAVIS_HOME/optimus
 pushd $TRAVIS_HOME/optimus
-mvn --quiet clean install -DskipTest=true
+mvn clean install -DskipTests=true
+popd
+
+git clone --depth=1 --branch=2.0.SP1 https://github.com/jimma/microprofile-rest-client.git $TRAVIS_HOME/microprofile-rest-client
+pushd $TRAVIS_HOME/microprofile-rest-client
+mvn clean install -DskipTests=true
 popd
 
 git clone --depth=1 --branch=master https://github.com/wildfly/wildfly.git $TRAVIS_HOME/wildfly
 pushd $TRAVIS_HOME/wildfly
-mvn --quiet clean install -DskipTest=true
+mvn clean install -DskipTests=true
 popd
