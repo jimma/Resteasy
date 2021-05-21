e7c21db0b740064b3e30a8d676ba71900e40fa80
Jim Ma <ema@redhat.com>
2021-05-21 18:26:48 +0800

    Quitely travis maven travis build

--- travis-script/prebuild.sh
+++ travis-script/prebuild.sh
@@ -2,7 +2,7 @@
 set -ex
 git clone --depth=1 --branch=ee9-experimental https://github.com/jimma/jboss-jakarta-jaxrs-api_spec.git $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec
 pushd $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec/jaxrs-api
-mvn clean install -DskipTests=true
+mvn --quiet clean install -DskipTests=true
 popd
 
 git clone --depth=1 --branch=main https://github.com/jimma/optimus.git $TRAVIS_HOME/optimus
@@ -12,10 +12,10 @@ popd
 
 git clone --depth=1 --branch=2.0.SP1 https://github.com/jimma/microprofile-rest-client.git $TRAVIS_HOME/microprofile-rest-client
 pushd $TRAVIS_HOME/microprofile-rest-client
-mvn clean install -DskipTests=true
+mvn --quiet clean install -DskipTests=true
 popd
 
 git clone --depth=1 --branch=master https://github.com/wildfly/wildfly.git $TRAVIS_HOME/wildfly
 pushd $TRAVIS_HOME/wildfly
-mvn clean install -DskipTests=true
+mvn --quiet clean install -DskipTests=true
 popd
