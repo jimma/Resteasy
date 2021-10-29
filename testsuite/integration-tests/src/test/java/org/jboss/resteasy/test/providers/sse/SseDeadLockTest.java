9a30ba8c61f44e9cb9c94ff848f65f8656f3de88
Jim Ma <ema@redhat.com>
2021-10-29 12:14:29 +0800

    [RESTEASY-3033]:Test for the sse dead lock

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/sse/SseDeadLockTest.java
@@ -0,0 +1,100 @@
+package org.jboss.resteasy.test.providers.sse;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.utils.PermissionUtil;
+import org.jboss.resteasy.utils.PortProviderUtil;
+import org.jboss.resteasy.utils.TestUtil;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.sse.SseEventSource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+public class SseDeadLockTest
+{
+
+    private static final Logger logger = Logger.getLogger(SseDeadLockTest.class);
+
+    @Deployment
+    public static Archive<?> deploy()
+    {
+        WebArchive war = TestUtil.prepareArchive(SseDeadLockTest.class.getSimpleName());
+        war.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        war.addAsManifestResource(PermissionUtil.createPermissionsXmlAsset(
+                new RuntimePermission("modifyThread")
+        ), "permissions.xml");
+        return TestUtil.finishContainerPrepare(war, null, Arrays.asList(SseResource.class),ExecutorServletContextListener.class);
+    }
+
+    private String generateURL(String path)
+    {
+        return PortProviderUtil.generateURL(path, SseDeadLockTest.class.getSimpleName());
+    }
+
+    @After
+    public void stopSendEvent() throws Exception
+    {
+        Client isOpenClient = ClientBuilder.newClient();
+        Invocation.Builder isOpenRequest = isOpenClient.target(generateURL("/server-sent-events/stopevent"))
+                .request();
+        isOpenRequest.get();
+
+    }
+
+
+    /**
+     * @tpTestDetails Test deadlock in sending of first sse events seen in 3.7.2
+     * @tpInfo RESTEASY-3033
+     * @tpSince RESTEasy 3.7.2
+     */
+    @Test
+    public void testDeadlockAtInitializationRepeated() throws Exception {
+        for (int i = 0; i < 10000; i++) {
+            try {
+                testDeadlockAtInitialization();
+            } catch(Exception e) {
+
+            }
+        }
+    }
+    public void testDeadlockAtInitialization() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final List<String> results = new ArrayList<String>();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(generateURL("/server-sent-events/initialization-deadlock"));
+        SseEventSource eventSource = SseEventSource.target(target).build();
+        eventSource.register(event -> {
+            String msg = event.readData(String.class);
+            results.add(msg);
+            if (msg.equals("last-msg")) {
+                latch.countDown();
+            }
+        }, ex -> {
+            logger.error(ex.getMessage(), ex);
+            throw new RuntimeException(ex);
+        });
+        eventSource.open();
+        boolean await = latch.await(10, TimeUnit.SECONDS);
+        //Assert.assertTrue("Waiting for event to be delivered has timed out.", await);
+        eventSource.close();
+        //Assert.assertFalse(eventSource.isOpen());
+        //Assert.assertEquals("first-msg", results.get(0));
+    }
+}
